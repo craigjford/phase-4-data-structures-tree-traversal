85b8fcb4f6009831dee14428142397a40d16d027
Craig Ford <craig_ford@icloud.com>
2022-11-26 19:11:26 -0500

    commit tree 983f0b5c743d79fa29e80683aad4cbaeda9998a2

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,45 @@
+require 'pry'
 class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  # def get_element_by_id(string)
+  #   # binding.pry
+
+  #   depth_first_traversal(string)
+  # end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+    # binding.pry
+    until nodes_to_visit.empty?
+      # binding.pry
+      current = nodes_to_visit.pop
+      # binding.pry
+      return current if current[:id] == id
+
+      nodes_to_visit = nodes_to_visit + current[:children]
+    end
+    nil
+  end
+
+  # def depth_first_traversal(node, result = [])
+
+  #   result.push(node[:value])
+  #   node[:children].each do |child|
+  #     depth_first_traversal(child, result)
+  #   end
+
+    # while nodes_to_visit.length > 0
+    #   node = nodes_to_visit.shift
+    #   result.push(node[:value])
+    #   nodes_to_visit = node[:children] + nodes_to_visit
+    # end
+
+  #   result
+  # end
 end
+
+# binding.pry
+# 0
